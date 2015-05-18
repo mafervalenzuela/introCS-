d926ea1f4f274dfe4ed2fa0e48252fe5738030cb
mafervalenzuela <18mvalenzuela@iaa.edu.ec>
2015-05-18 09:45:46 -0500

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,29 @@
+name = "Mafer Valenzuela" #sting
+age = 14 #integer
+gpa = 4.0 #floating point
+
+print name
+print age
+print gpa
+
+print "My name is", name, ". I am ", age, "years old. "
+
+print "In ten years, I will be", age + 10
+
+#Dog years
+ageInHumanYears = 3
+ageInDogYears = ageInHumanYears * 7
+
+print 'My dog is', ageInDogYears, 'years old in Dog years.'
+print 'But my dog is', ageInHumanYears, 'years old in Human years.'
+
+#order of operations
+#parenthesis, Exponents, Multiplication, Divison, Addition, Substraction
+myNumber = 5 * 4 + 10
+print myNumber
+
+print "Hello", 2
+
+import math
+root = math. sqrt (81) 
+print root
