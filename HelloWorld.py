196567755d521f651f52487b517b4381d460720b
mafervalenzuela <18mvalenzuela@iaa.edu.ec>
2015-05-18 09:44:41 -0500

    Create HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,21 @@
+print 'Hello World'
+print 'My Name is Mafer' 
+print 'I am 14 years old \nThis is Python' 
+
+print 2015 + 1 #Printing an integer
+print '2015'
+
+print 3.1416 #Printing a floating point number
+
+print 2015 - 1 #Printing an integer 
+
+print 2015 * 2 #Printing an integer
+
+print 81 / 9 #Printing an integer 
+
+#Module
+print 10%3 
+
+print "Odd or Even Numbers"
+print 17 % 2 
+print 16 % 2 
