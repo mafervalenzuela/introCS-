e425bbe6f5469b033d9ec624acec05822e283bae
mafervalenzuela <18mvalenzuela@iaa.edu.ec>
2015-05-20 09:28:51 -0500

    Create commandLine.py

---
+++ commandLine.py
@@ -0,0 +1,13 @@
+name = "Mafer"
+print "My name is", name
+
+import sys
+print "Your name is: ", sys.argv [1]
+print "Your middle name is: ", sys.argv [2]
+print "Your last name is: ", sys.argv [3]
+
+# print first name
+#print last name
+#print your year of birth
+#print your year of graduation
+#print your GPA
