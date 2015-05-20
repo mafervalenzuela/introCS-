b28ee54e015aa612cea6a73eeab42b33d147d92e
mafervalenzuela <18mvalenzuela@iaa.edu.ec>
2015-05-20 09:52:57 -0500

    Create Homework1.py

---
+++ Homework1.py
@@ -0,0 +1,8 @@
+import sys
+print "Your first name is", sys. argv [1]
+print "Your last name is", sys. argv [2]
+print "Your year of birth is", sys. argv [3] 
+print "Your year of graduation is", sys. argv [4]
+print "Your GPA is", sys. argv [5]
+
+
