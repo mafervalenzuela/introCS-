2f946a5284d6c246b9e9c89ab12d2b5a6b7c4740
mafervalenzuela <18mvalenzuela@iaa.edu.ec>
2015-05-20 21:05:14 -0500

    Create Practice2.py

---
+++ Practice2.py
@@ -0,0 +1,19 @@
+name = raw_input( "What is your name?")
+last = raw_input("What is your last name?") 
+
+print "Your name is", name, last
+
+age = input("When is your year of birth?")
+
+print "Your year of birth is", age
+
+singer = raw_input(" Who is your favorite singer?")
+movie = raw_input(" What is your favorite movie?")
+best = raw_input(" Who is your best friend?")
+
+print "Your favorite singer is", singer
+print "Your favorite movie is", movie
+print "Your best friend is", best 
+
+current = 2015-2000
+print "My current age is", current
