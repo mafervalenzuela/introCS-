051fe874d2f62d18021b609e832b9e87354da8d2
mafervalenzuela <18mvalenzuela@iaa.edu.ec>
2015-05-28 09:36:24 -0500

    Create Conditionals.py

---
+++ Conditionals.py
@@ -0,0 +1,11 @@
+print "Bar"
+age = input ("What is the age the costumer?")
+
+if age >= 18:
+    print "The customer is legally allowed to drink liquor"
+    print "You may serve"
+else:
+     print "The customer is NOT legally allowed to drink liquor"
+     
+    
+
